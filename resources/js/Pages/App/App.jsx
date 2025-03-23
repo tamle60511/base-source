236e8470d71599f942886a9ee5e261dc8281c3b5
tamle60511 <103232155+tamle60511@users.noreply.github.com>
2025-03-23 12:33:59 +0700

    new code

---
+++ resources/js/Pages/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePage from './Templates/HomePage'
+
+function Home() {
+  return (
+    <div >
+      <HomePage />
+    </div>
+  )
+}
+
+export default Home
---
+++ resources/js/Pages/App/Constants/navigation.jsx
@@ -0,0 +1,160 @@
+export const navigation = [
+    {
+        id: "0",
+        title: "Products",
+        url: "/products",
+        dropdown: [
+            {
+                id: "0",
+                title: "AL. Wheel",
+                url: "/al-wheel",
+            },
+            {
+                id: "1",
+                title: "Light - Weight Products",
+                url: "/light-weight-products",
+            },
+            {
+                id: "2",
+                title: "Green Energy Products",
+                url: "/green-energy-products",
+            },
+            {
+                id: "3",
+                title: "Agricultural Products",
+                url: "/agricultural-products",
+            },
+        ]
+    },
+    {
+        id: "1",
+        title: "Resources",
+        url: "/resources",
+    },
+    {
+        id: "2",
+        title: "News",
+        url: "/news",
+    },
+    {
+        id: "3",
+        title: "About Us",
+        url: "/about-us",
+    },
+    {
+        id: "4",
+        title: "Contact",
+        url: "/contact",
+    },
+];
+
+export const menu = [
+    {
+        id: "0",
+        title: "More information",
+        items: [
+            {
+                id: "0",
+                title: "About Us",
+                url: "/about-us",
+            },
+            {
+                id: "1",
+                title: "Contact Us",
+                url: "/contact-us",
+            },
+            {
+                id: "2",
+                title: "Gift Card",
+                url: "/vouchers",
+            },
+            {
+                id: "3",
+                title: "Careers",
+                url: "/career",
+            },
+            {
+                id: "4",
+                title: "Privacy Notice",
+                url: "/policy",
+            },
+            {
+                id: "5",
+                title: "Legal Noice",
+                url: "/policy",
+            },
+            {
+                id: "6",
+                title: "Leadership",
+                url: "/history",
+            },
+        ],
+    },
+    {
+        id: "1",
+        title: "Our Products",
+        items: [
+            {
+                id: "0",
+                title: "Essentials",
+                url: "/",
+            },
+            {
+                id: "1",
+                title: "e-Gifting",
+                url: "/",
+            },
+            {
+                id: "2",
+                title: "Accommodation",
+                url: "/accommodation",
+            },
+            {
+                id: "3",
+                title: "Dining",
+                url: "/dining",
+            },
+            {
+                id: "4",
+                title: "Spa & Fitness",
+                url: "/wellness",
+            },
+            {
+                id: "5",
+                title: "Offers",
+                url: "/special-offers",
+            },
+            {
+                id: "6",
+                title: "Events",
+                url: "/wedding-and-events",
+            },
+        ],
+    },
+    {
+        id: "2",
+        title: "Other Page",
+        items: [
+            {
+                id: "0",
+                title: "Reservation",
+                url: "/reservation",
+            },
+            {
+                id: "1",
+                title: "Image Gallery",
+                url: "/gallery",
+            },
+            {
+                id: "2",
+                title: "Cookie Preferences",
+                url: "/",
+            },
+            {
+                id: "3",
+                title: "Best Rate Guarantee",
+                url: "/",
+            },
+        ],
+    },
+];
